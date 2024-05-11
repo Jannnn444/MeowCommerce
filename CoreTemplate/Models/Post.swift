be9b45c3a6ec9c33d6faa36e720ab9268876c387
Jannnn444 <138713229+Jannnn444@users.noreply.github.com>
2024-05-12 00:56:27 +0800

    first commit

---
+++ CoreTemplate/Models/Post.swift
@@ -0,0 +1,8 @@
+//
+//  Post.swift
+//  CoreTemplate
+//
+//  Created by Kranti on 2024/4/29.
+//
+
+import Foundation
---
+++ CoreTemplate/config.swift
@@ -0,0 +1,8 @@
+//
+//  config.swift
+//  CoreTemplate
+//
+//  Created by Kranti on 2024/5/12.
+//
+
+import Foundation
